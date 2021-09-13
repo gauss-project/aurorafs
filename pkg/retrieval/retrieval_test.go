2924d1e1852e17fbe3aad28a6a6d012d01f608dd
HoffmanAufs <88414654+HoffmanAufs@users.noreply.github.com>
2021-09-13 15:51:34 +0800

    feat:retrieval dev (#20)

--- pkg/retrieval/retrieval_test.go
+++ pkg/retrieval/retrieval_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gauss-project/aurorafs/pkg/aurora"
 	"github.com/gauss-project/aurorafs/pkg/logging"
 	"github.com/gauss-project/aurorafs/pkg/routetab"
+
 	// "github.com/gauss-project/aurorafs/pkg/cac"
 	// "github.com/gauss-project/aurorafs/pkg/soc"
 
@@ -28,6 +29,7 @@ import (
 	"github.com/gauss-project/aurorafs/pkg/storage"
 	storemock "github.com/gauss-project/aurorafs/pkg/storage/mock"
 	"github.com/gauss-project/aurorafs/pkg/topology"
+
 	// "io"
 	// "os"
 	// "github.com/sirupsen/logrus"
@@ -63,7 +65,7 @@ func TestDelivery(t *testing.T) {
 
 	// create the server that will handle the request and will serve the response
 	mockRouteTable := NewMockRouteTable()
-	server := retrieval.New(serverAddr, nil, &mockRouteTable, mockStorer, logger, nil)
+	server := retrieval.New(serverAddr, nil, &mockRouteTable, mockStorer, true, logger, nil)
 	serverMockChunkInfo := NewMockChunkInfo()
 	server.Config(serverMockChunkInfo)
 
@@ -87,7 +89,7 @@ func TestDelivery(t *testing.T) {
 	mockChunkInfo := NewMockChunkInfo()
 	mockChunkInfo.OnChunkTransferred(chunk.Address(), rootAddr, serverAddr)
 
-	client := retrieval.New(clientAddr, recorder, &mockRouteTable, clientMockStorer, logger, nil)
+	client := retrieval.New(clientAddr, recorder, &mockRouteTable, clientMockStorer, true, logger, nil)
 	client.Config(mockChunkInfo)
 	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
 	defer cancel()
@@ -158,14 +160,21 @@ func TestDelivery(t *testing.T) {
 	// }
 }
 
-func TestRetrievaeChunk(t *testing.T) {
+func TestRetrievalChunk(t *testing.T) {
+	// logger := logging.New(io.MultiWriter(os.Stdout), 6)
+	// logFormater := logrus.TextFormatter{
+	// 	DisableTimestamp: true,
+	// 	ForceColors:      true,
+	// }
+	// logger.NewEntry().Logger.SetFormatter(&logFormater)
+	logger := logging.New(ioutil.Discard, 0)
 	var (
-		logger = logging.New(ioutil.Discard, 0)
+		mockRouteTable = NewMockRouteTable()
 	)
 
 	t.Run("downstream", func(t *testing.T) {
-		serverAddress := boson.MustParseHexAddress("03")
-		clientAddress := boson.MustParseHexAddress("01")
+		serverAddress := boson.MustParseHexAddress("0003")
+		clientAddress := boson.MustParseHexAddress("0001")
 		// chunk := testingc.FixtureChunk("02c2")
 
 		chunkBytes := []uint8{3, 0, 0, 0, 0, 0, 0, 0, 102, 111, 111}
@@ -183,27 +192,19 @@ func TestRetrievaeChunk(t *testing.T) {
 		}
 
 		serverMockChunkInfo := NewMockChunkInfo()
-		mockRouteTable := NewMockRouteTable()
-		server := retrieval.New(serverAddress, nil, &mockRouteTable, serverStorer, logger, nil)
+		server := retrieval.New(serverAddress, nil, &mockRouteTable, serverStorer, true, logger, nil)
 		server.Config(serverMockChunkInfo)
 
-		// recorder := streamtest.New(streamtest.WithProtocols(server.Protocol()))
 		recorder := streamtest.New(
 			streamtest.WithProtocols(server.Protocol()),
 			streamtest.WithBaseAddr(clientAddress),
 		)
 
-		// clientSuggester := mockPeerSuggester{eachPeerRevFunc: func(f topology.EachPeerFunc) error {
-		// 	_, _, _ = f(serverAddress, 0)
-		// 	return nil
-		// }}
-
 		clientStorer := storemock.NewStorer()
 		clientMockChunkInfo := NewMockChunkInfo()
 		clientMockChunkInfo.OnChunkTransferred(chunk.Address(), rootAddr, serverAddress)
-		client := retrieval.New(clientAddress, recorder, &mockRouteTable, clientStorer, logger, nil)
+		client := retrieval.New(clientAddress, recorder, &mockRouteTable, clientStorer, true, logger, nil)
 		client.Config(clientMockChunkInfo)
-		// client := retrieval.New(clientAddress, nil, recorder, clientSuggester, logger, accountingmock.NewAccounting(), pricer, nil, false, noopStampValidator)
 
 		got, err := client.RetrieveChunk(context.Background(), rootAddr, chunk.Address())
 		if err != nil {
@@ -222,9 +223,9 @@ func TestRetrievaeChunk(t *testing.T) {
 		chunk := boson.NewChunk(address, data)
 		rootAddr := boson.MustParseHexAddress("0101")
 
-		serverAddress := boson.MustParseHexAddress("0100000000000000000000000000000000000000000000000000000000000000")
-		forwarderAddress := boson.MustParseHexAddress("0200000000000000000000000000000000000000000000000000000000000000")
-		clientAddress := boson.MustParseHexAddress("030000000000000000000000000000000000000000000000000000000000000000")
+		serverAddress := boson.MustParseHexAddress("0001")
+		forwarderAddress := boson.MustParseHexAddress("0002")
+		clientAddress := boson.MustParseHexAddress("0003")
 
 		// config server
 		serverStorer := storemock.NewStorer()
@@ -232,11 +233,7 @@ func TestRetrievaeChunk(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		mockRouteTable := NewMockRouteTable()
-		server := retrieval.New(serverAddress, nil, &mockRouteTable, serverStorer, logger, nil)
-
-		serverChunkInfo := NewMockChunkInfo()
-		server.Config(serverChunkInfo)
+		server := retrieval.New(serverAddress, nil, &mockRouteTable, serverStorer, true, logger, nil)
 
 		// config forwarder
 		f2sRecorder := streamtest.New(
@@ -244,11 +241,7 @@ func TestRetrievaeChunk(t *testing.T) {
 			streamtest.WithBaseAddr(forwarderAddress),
 		)
 		forwarderStorer := storemock.NewStorer()
-		forwarder := retrieval.New(forwarderAddress, f2sRecorder, &mockRouteTable, forwarderStorer, logger, nil)
-
-		forwarderChunkInfo := NewMockChunkInfo()
-		forwarderChunkInfo.OnChunkTransferred(chunk.Address(), rootAddr, serverAddress)
-		forwarder.Config(forwarderChunkInfo)
+		forwarder := retrieval.New(forwarderAddress, f2sRecorder, &mockRouteTable, forwarderStorer, true, logger, nil)
 
 		// config client
 		c2fRecorder := streamtest.New(
@@ -256,10 +249,10 @@ func TestRetrievaeChunk(t *testing.T) {
 			streamtest.WithBaseAddr(clientAddress),
 		)
 		clientStorer := storemock.NewStorer()
-		client := retrieval.New(clientAddress, c2fRecorder, &mockRouteTable, clientStorer, logger, nil)
+		client := retrieval.New(clientAddress, c2fRecorder, &mockRouteTable, clientStorer, true, logger, nil)
 
 		clientChunkInfo := NewMockChunkInfo()
-		clientChunkInfo.OnChunkTransferred(chunk.Address(), rootAddr, forwarderAddress)
+		clientChunkInfo.OnChunkTransferred(chunk.Address(), rootAddr, serverAddress)
 		client.Config(clientChunkInfo)
 
 		if got, _ := forwarderStorer.Has(context.Background(), storage.ModeHasRetrievalData, chunk.Address()); got {
@@ -276,6 +269,88 @@ func TestRetrievaeChunk(t *testing.T) {
 
 		if got, _ := forwarderStorer.Has(context.Background(), storage.ModeHasRetrievalData, chunk.Address()); !got {
 			t.Fatalf("forwarder did not cache chunk")
+		}
+	})
+
+}
+
+func TestNeighborRetrieval(t *testing.T) {
+	// logger := logging.New(io.MultiWriter(os.Stdout), 6)
+	// logFormater := logrus.TextFormatter{
+	// 	DisableTimestamp: true,
+	// 	ForceColors:      true,
+	// }
+	// logger.NewEntry().Logger.SetFormatter(&logFormater)
+
+	logger := logging.New(ioutil.Discard, 0)
+
+
+	address := boson.MustParseHexAddress("8a74889a73c23fe2be037886c6b709e3175b95b8deea9c95eeda0dbc60740bd8")
+	data := []uint8{3, 0, 0, 0, 0, 0, 0, 0, 102, 111, 111}
+
+	chunk := boson.NewChunk(address, data)
+	chunkAddr := chunk.Address()
+	rootAddr := boson.MustParseHexAddress("1001")
+
+	t.Run("relay node contains chunk", func(t *testing.T) {
+		// client retrieval => relay => server
+		serverAddress := boson.MustParseHexAddress("0001")
+		neighborServerAddress := boson.MustParseHexAddress("0002")
+		clientAddress := boson.MustParseHexAddress("0003")
+
+		// init server
+		serverStorer := storemock.NewStorer()
+		if _, err := serverStorer.Put(context.Background(), storage.ModePutUpload, chunk); err != nil {
+			t.Fatal(err)
+		}
+		server := retrieval.New(serverAddress, nil, &mockRouteTable{}, serverStorer, true, logger, nil)
+		neighbor2serverRecorder := streamtest.New(
+			streamtest.WithProtocols(
+				server.Protocol(),
+			),
+			streamtest.WithBaseAddr(
+				neighborServerAddress,
+			),
+		)
+
+		// init neighbor
+		neighborStorer := storemock.NewStorer()
+		// if _, err := relayStorer.Put(context.Background(), storage.ModePutUpload, chunk); err != nil{
+		// 	t.Fatal(err)
+		// }
+		neighborServer := retrieval.New(neighborServerAddress, neighbor2serverRecorder, &mockRouteTable{}, neighborStorer, true, logger, nil)
+		client2neighborRecorder := streamtest.New(
+			streamtest.WithProtocols(
+				neighborServer.Protocol(),
+			),
+			streamtest.WithBaseAddr(
+				clientAddress,
+			),
+		)
+
+		// init client
+		clientRouteTable := mockRouteTable{
+			rejectAddrList: []boson.Address{serverAddress},
+			neighborMap: map[string][]boson.Address{
+				serverAddress.String(): {neighborServerAddress},
+			},
+		}
+		client := retrieval.New(clientAddress, client2neighborRecorder, &clientRouteTable, nil, true, logger, nil)
+
+		clientChunkInfo := NewMockChunkInfo()
+		clientChunkInfo.OnChunkTransferred(chunkAddr, rootAddr, serverAddress)
+		client.Config(clientChunkInfo)
+
+		got, err := client.RetrieveChunk(context.Background(), rootAddr, chunk.Address())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(got.Data(), chunk.Data()) {
+			t.Fatalf("got data %x, want %x", got.Data(), chunk.Data())
+		}
+
+		if got, _ := neighborStorer.Has(context.Background(), storage.ModeHasRetrievalData, chunk.Address()); !got {
+			t.Fatalf("neighbor did not cache chunk")
 		}
 	})
 }
@@ -316,8 +391,8 @@ func TestRetrievePreemptiveRetry(t *testing.T) {
 
 	clientAddress := boson.MustParseHexAddress("1010")
 
-	serverAddress1 := boson.MustParseHexAddress("111111")
-	serverAddress2 := boson.MustParseHexAddress("222222")
+	serverAddress1 := boson.MustParseHexAddress("0001")
+	serverAddress2 := boson.MustParseHexAddress("0002")
 
 	serverStorer1 := storemock.NewStorer()
 	serverStorer2 := storemock.NewStorer()
@@ -334,12 +409,18 @@ func TestRetrievePreemptiveRetry(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	mockRouteTable := NewMockRouteTable()
-	server1 := retrieval.New(serverAddress1, nil, &mockRouteTable, serverStorer1, logger, nil)
+	defaultMockRouteTable := NewMockRouteTable()
+
+	server1RouteTable := mockRouteTable{
+		rejectAddrList: []boson.Address{serverAddress2},
+		neighborMap: map[string][]boson.Address{},
+	}
+
+	server1 := retrieval.New(serverAddress1, nil, &server1RouteTable, serverStorer1, true, logger, nil)
 	server1ChunkInfo := NewMockChunkInfo()
 	server1.Config(server1ChunkInfo)
 
-	server2 := retrieval.New(serverAddress2, nil, &mockRouteTable, serverStorer2, logger, nil)
+	server2 := retrieval.New(serverAddress2, nil, &defaultMockRouteTable, serverStorer2, true, logger, nil)
 	server2ChunkInfo := NewMockChunkInfo()
 	server2.Config(server2ChunkInfo)
 
@@ -369,14 +450,13 @@ func TestRetrievePreemptiveRetry(t *testing.T) {
 			),
 		)
 
-		client := retrieval.New(clientAddress, recorder, &mockRouteTable, nil, logger, nil)
+		client := retrieval.New(clientAddress, recorder, &defaultMockRouteTable, nil, true, logger, nil)
 		clientChunkInfo := NewMockChunkInfo()
 
 		clientChunkInfo.OnChunkTransferred(chunk.Address(), chunkRootAddr, serverAddress2)
 		client.Config(clientChunkInfo)
 
 		got, err := client.RetrieveChunk(context.Background(), chunkRootAddr, chunk.Address())
-		// got, err = client.RetrieveChunk(context.Background(), chunkRootAddr, chunk.Address())
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -414,7 +494,7 @@ func TestRetrievePreemptiveRetry(t *testing.T) {
 
 		// client := retrieval.New(clientAddress, nil, recorder, peerSuggesterFn(peers...), logger, accountingmock.NewAccounting(), pricerMock, nil, false, noopStampValidator)
 		mockRouteTable := NewMockRouteTable()
-		client := retrieval.New(clientAddress, recorder, &mockRouteTable, nil, logger, nil)
+		client := retrieval.New(clientAddress, recorder, &mockRouteTable, nil, true, logger, nil)
 
 		clientChunkInfo := NewMockChunkInfo()
 		clientChunkInfo.OnChunkTransferred(chunk.Address(), chunkRootAddr, serverAddress1)
@@ -430,31 +510,46 @@ func TestRetrievePreemptiveRetry(t *testing.T) {
 			t.Fatalf("got data %x, want %x", got.Data(), chunk.Data())
 		}
 	})
-
 }
 
 type mockRouteTable struct {
+	rejectAddrList []boson.Address
+	neighborMap    map[string][]boson.Address
 }
+
+// func (r *mockRouteTable)appendRejectAddress(addr boson.Address){
+// 	r.rejectAddrList = append(r.rejectAddrList, addr)
+// }
 
 func NewMockRouteTable() mockRouteTable {
 	return mockRouteTable{}
 }
 
 func (r *mockRouteTable) GetRoute(ctx context.Context, target boson.Address) (dest *aurora.Address, routes []routetab.RouteItem, err error) {
-	return nil, []routetab.RouteItem{}, nil
-}
-func (r *mockRouteTable) FindRoute(ctx context.Context, target boson.Address) (dest *aurora.Address, route []routetab.RouteItem, err error) {
-	return nil, []routetab.RouteItem{}, nil
-}
-func (r *mockRouteTable) Connect(ctx context.Context, target boson.Address) error {
-	return nil
+	return
 }
 
-func (r *mockRouteTable) GetTargetNeighbor(ctx context.Context, target boson.Address) (addresses []boson.Address, err error) {
-	return nil, nil
+func (r *mockRouteTable) FindRoute(ctx context.Context, target boson.Address) (dest *aurora.Address, route []routetab.RouteItem, err error) {
+	return
 }
+
+func (r *mockRouteTable) Connect(ctx context.Context, target boson.Address) (err error) {
+	for _, node := range r.rejectAddrList {
+		if target.Equal(node) {
+			return fmt.Errorf("reject")
+		}
+	}
+	return
+}
+
+func (r *mockRouteTable) GetTargetNeighbor(ctx context.Context, target boson.Address, limit int) ([]boson.Address, error) {
+	// nodeList := make([]boson.Address, 0)
+	// return nodeList, nil
+	return r.neighborMap[target.String()], nil
+}
+
 func (r *mockRouteTable) IsNeighbor(dest boson.Address) (has bool) {
-	return false
+	return
 }
 
 type mockPeerSuggester struct {
@@ -479,7 +574,14 @@ func NewMockChunkInfo() *mockChunkInfo {
 	}
 }
 
-func (c *mockChunkInfo) FindChunkInfo(ctx context.Context, authInfo []byte, rootCid boson.Address, overlays []boson.Address) {
+func (c *mockChunkInfo) DelFile(rootCid, overlay boson.Address) bool{
+	return false
+}
+func (c *mockChunkInfo) GetFileList(overlay boson.Address) (fileListInfo map[string]*aurora.FileInfo, rootList []boson.Address){
+	return
+}
+func (c *mockChunkInfo) FindChunkInfo(ctx context.Context, authInfo []byte, rootCid boson.Address, overlays []boson.Address) bool{
+	return false
 }
 
 func (c *mockChunkInfo) GetChunkInfo(rootCid boson.Address, cid boson.Address) [][]byte {
@@ -510,14 +612,6 @@ func (c *mockChunkInfo) IsDiscover(rootCid boson.Address) bool {
 	return false
 }
 
-func (c *mockChunkInfo) GetFileList(overlay boson.Address) (fileListInfo map[string]*aurora.FileInfo, rootList []boson.Address) {
-	return nil, nil
-}
-
-func (c *mockChunkInfo) DelFile(rootCid, overlay boson.Address) bool {
-	return true
-}
-
 // hasher is a helper function to hash a given data based on the given span.
 func hasher(data []byte) func([]byte) ([]byte, error) {
 	return func(span []byte) ([]byte, error) {
@@ -535,35 +629,6 @@ func hasher(data []byte) func([]byte) ([]byte, error) {
 }
 
 func TestBasic(t *testing.T) {
-	// bmtHashOfFoo := "8a74889a73c23fe2be037886c6b709e3175b95b8deea9c95eeda0dbc60740bd8"
-	// bmtHashOfFoo := "2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48"
-	// address := boson.MustParseHexAddress(bmtHashOfFoo)
-
-	// foo := "foo"
-	// fooLength := len(foo)
-	// fooBytes := make([]byte, boson.SpanSize+fooLength)
-	// binary.LittleEndian.PutUint64(fooBytes, uint64(fooLength))
-	// copy(fooBytes[boson.SpanSize:], foo)
-	// var fixtureChunks = map[string]boson.Chunk{
-	// 	"c8ea": boson.NewChunk(
-	// 		boson.MustParseHexAddress("c8eaf98c8a8d62c6591d00f9d7b306805c5c0953b706d9ce8c66b90ba230687b"),
-	// 		// boson.MustParseHexAddress("0025737be11979e91654dffd2be817ac1e52a2dadb08c97a7cef12f937e707bc"),
-	// 		[]byte{72, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 149, 179, 31, 244, 146, 247, 129, 123, 132, 248, 215, 77, 44, 47, 91, 248, 229, 215, 89, 156, 210, 243, 3, 110, 204, 74, 101, 119, 53, 53, 145, 188, 193, 153, 130, 197, 83, 152, 36, 140, 150, 209, 191, 214, 193, 4, 144, 121, 32, 45, 205, 220, 59, 227, 28, 43, 161, 51, 108, 14, 106, 180, 135, 2},
-	// 	),
-	// 	"2989": boson.NewChunk(
-	// 		boson.MustParseHexAddress("2989f0cb15303f352231c35585a4f8012e67bdb20ab337d98c545e4c9dda395f"),
-	// 		[]byte{72, 0, 0, 0, 0, 0, 0, 0, 170, 117, 0, 0, 0, 0, 0, 0, 21, 157, 63, 86, 45, 17, 166, 184, 47, 126, 58, 172, 242, 77, 153, 249, 97, 5, 107, 244, 23, 153, 220, 255, 254, 47, 209, 24, 63, 58, 126, 142, 41, 79, 201, 182, 178, 227, 235, 223, 63, 11, 220, 155, 40, 181, 56, 204, 91, 44, 51, 185, 95, 155, 245, 235, 187, 250, 103, 49, 139, 184, 46, 199},
-	// 	),
-	// 	"4b4e": boson.NewChunk(
-	// 		boson.MustParseHexAddress("4b4e98bceed166e1089b79ca6bff553c9dba00bd016c8a4c98bb379f6065a688"),
-	// 		[]byte{72, 0, 0, 0, 0, 0, 0, 0, 226, 0, 0, 0, 0, 0, 0, 0, 67, 234, 252, 231, 229, 11, 121, 163, 131, 171, 41, 107, 57, 191, 221, 32, 62, 204, 159, 124, 116, 87, 30, 244, 99, 137, 121, 248, 119, 56, 74, 102, 140, 73, 178, 7, 151, 22, 47, 126, 173, 30, 43, 7, 61, 187, 13, 236, 59, 194, 245, 18, 25, 237, 106, 125, 78, 241, 35, 34, 116, 154, 105, 205},
-	// 	),
-	// 	"f4c4": boson.NewChunk(
-	// 		boson.MustParseHexAddress("f4c4521e86ba4e00780a37c4f34b7f7162de732bf7ce31d54c8f753ded04fc39"),
-	// 		[]byte{72, 0, 0, 0, 0, 0, 0, 0, 124, 59, 0, 0, 0, 0, 0, 0, 44, 67, 19, 101, 42, 213, 4, 209, 212, 189, 107, 244, 111, 22, 230, 24, 245, 103, 227, 165, 88, 74, 50, 11, 143, 197, 220, 118, 175, 24, 169, 193, 15, 40, 225, 196, 246, 151, 1, 45, 86, 7, 36, 99, 156, 86, 83, 29, 46, 207, 115, 112, 126, 88, 101, 128, 153, 113, 30, 27, 50, 232, 77, 215},
-	// 	),
-	// }
-
 	// chunkByteList := [][]byte{
 	// 	[]byte{72, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 149, 179, 31, 244, 146, 247, 129, 123, 132, 248, 215, 77, 44, 47, 91, 248, 229, 215, 89, 156, 210, 243, 3, 110, 204, 74, 101, 119, 53, 53, 145, 188, 193, 153, 130, 197, 83, 152, 36, 140, 150, 209, 191, 214, 193, 4, 144, 121, 32, 45, 205, 220, 59, 227, 28, 43, 161, 51, 108, 14, 106, 180, 135, 2},
 	// 	[]byte{72, 0, 0, 0, 0, 0, 0, 0, 170, 117, 0, 0, 0, 0, 0, 0, 21, 157, 63, 86, 45, 17, 166, 184, 47, 126, 58, 172, 242, 77, 153, 249, 97, 5, 107, 244, 23, 153, 220, 255, 254, 47, 209, 24, 63, 58, 126, 142, 41, 79, 201, 182, 178, 227, 235, 223, 63, 11, 220, 155, 40, 181, 56, 204, 91, 44, 51, 185, 95, 155, 245, 235, 187, 250, 103, 49, 139, 184, 46, 199},
