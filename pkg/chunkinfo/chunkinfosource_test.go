31939bb91730cff3a491327fecc104c0daaeb7cc
vicentaufs <vicent@aufs.io>
2021-11-10 16:55:25 +0800

    Feat: Add the source information of file chunks to ChunkInfo/DebugApi/Retrieval  * feat(chunkinfo):Add the persistent storage function of source information for file chuncks and pyramid to ChunkInfo  * fix(retrieval): Submit the source information of file chuncks to ChunkInfo  * feat(debugApi): Add the chunk/source open interfaces  * docs: update openapi  * docs: update version

---
+++ pkg/chunkinfo/chunkinfosource_test.go
@@ -0,0 +1,81 @@
+package chunkinfo
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/gauss-project/aurorafs/pkg/boson"
+	"github.com/gauss-project/aurorafs/pkg/p2p"
+	"github.com/gauss-project/aurorafs/pkg/p2p/streamtest"
+	"golang.org/x/sync/errgroup"
+	"testing"
+)
+
+func TestChunkInfoSource(t *testing.T) {
+	clientAddress := boson.MustParseHexAddress("01")
+	cid := boson.MustParseHexAddress("02")
+	rootCid, s := mockUploadFile(t)
+	recorder := streamtest.New(
+		streamtest.WithProtocols(
+			newTestProtocol(func(ctx context.Context, peer p2p.Peer, stream p2p.Stream) error {
+				if _, err := bufio.NewReader(stream).ReadString('\n'); err != nil {
+					return err
+				}
+				var g errgroup.Group
+				g.Go(stream.Close)
+				g.Go(stream.FullClose)
+
+				if err := g.Wait(); err != nil {
+					return err
+				}
+				return stream.FullClose()
+			}, protocolName, protocolVersion, streamChunkInfoReqName)),
+	)
+
+	a := mockChunkInfo(s, recorder, clientAddress)
+
+	if err := a.UpdateChunkInfoSource(rootCid, clientAddress, cid); err != nil {
+		t.Fatal(err)
+	}
+
+	source := a.GetChunkInfoSource(rootCid)
+	if len(source.ChunkSource) == 0 {
+		t.Fatal(" Get chunk source to zero.")
+	}
+	err := a.InitChunkInfo()
+	if err != nil {
+		t.Fatal("ChunkInfo Init Failed:", err.Error())
+	}
+
+	delfile := a.DelFile(rootCid)
+	if !delfile {
+		t.Fatal("delete file error")
+	}
+
+	source = a.GetChunkInfoSource(rootCid)
+	if len(source.ChunkSource) != 0 || source.PyramidSource != "" {
+		t.Fatal("Failed to delete source record.")
+	}
+
+	if err := a.UpdatePyramidSource(context.Background(), rootCid, clientAddress); err != nil {
+		t.Fatal(err)
+	}
+
+	source = a.GetChunkInfoSource(rootCid)
+	if source.PyramidSource != clientAddress.String() {
+		t.Fatal("Pyramid values are unequal.")
+	}
+
+	delfile = a.DelFile(rootCid)
+	if !delfile {
+		t.Fatal("delete file error")
+	}
+
+	source = a.GetChunkInfoSource(rootCid)
+	if len(source.ChunkSource) != 0 || source.PyramidSource != "" {
+		t.Fatal("Failed to delete source record.")
+	}
+
+	fmt.Println("Test Success")
+
+}
