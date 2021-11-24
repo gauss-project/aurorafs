3cd2c92f21e72600c7ab643ec8884ea657f28bed
lewis <Lewis@aufs.io>
2021-11-24 11:46:16 +0800

    feat(traffic): init traffic.

--- pkg/settlement/traffic/cheque/chequestore.go
+++ pkg/settlement/traffic/cheque/chequestore.go
@@ -8,15 +8,13 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gauss-project/aurorafs/pkg/crypto"
-	"github.com/gauss-project/aurorafs/pkg/settlement/chain/transaction"
 	"github.com/gauss-project/aurorafs/pkg/storage"
 )
 
 var (
-	// ErrNoCheque is the error returned if there is no prior cheque for a chequebook or beneficiary.
+	// ErrNoCheque is the error returned if there is no prior cheque for a chainAddress or beneficiary.
 	ErrNoCheque = errors.New("no cheque")
 	// ErrChequeNotIncreasing is the error returned if the cheque amount is the same or lower.
 	ErrChequeNotIncreasing = errors.New("cheque cumulativePayout is not increasing")
@@ -24,29 +22,28 @@ var (
 	ErrChequeInvalid = errors.New("invalid cheque")
 	// ErrWrongBeneficiary is the error returned if the cheque has the wrong beneficiary.
 	ErrWrongBeneficiary = errors.New("wrong beneficiary")
-	// ErrBouncingCheque is the error returned if the chequebook is demonstrably illiquid.
-	ErrBouncingCheque        = errors.New("bouncing cheque")
-	lastReceivedChequePrefix = "swap_chequebook_last_received_cheque_"
+	// ErrBouncingCheque is the error returned if the chainAddress is demonstrably illiquid.
+	ErrBouncingCheque                  = errors.New("bouncing cheque")
+	lastTransferredTrafficChequePrefix = "transferred_traffic_cheque_"
+	lastRetrievedTrafficChequePrefix   = "retrieved_traffic_cheque_"
 )
 
 // ChequeStore handles the verification and storage of received cheques
 type ChequeStore interface {
 	// ReceiveCheque verifies and stores a cheque. It returns the totam amount earned.
 	ReceiveCheque(ctx context.Context, cheque *SignedCheque) (*big.Int, error)
-	// LastCheque returns the last cheque we received from a specific chequebook.
-	LastCheque(chequebook common.Address) (*SignedCheque, error)
-	// LastCheques returns the last received cheques from every known chequebook.
+	// LastCheque returns the last cheque we received from a specific chainAddress.
+	LastCheque(chainAddress common.Address) (*SignedCheque, error)
+	// LastCheques returns the last received cheques from every known chainAddress.
 	LastCheques() (map[common.Address]*SignedCheque, error)
 }
 
 type chequeStore struct {
-	lock                  sync.Mutex
-	store                 storage.StateStorer
-	chaindID              int64
-	simpleSwapBindingFunc SimpleSwapBindingFunc
-	backend               transaction.Backend
-	beneficiary           common.Address // the beneficiary we expect in cheques sent to us
-	recoverChequeFunc     RecoverChequeFunc
+	lock              sync.Mutex
+	store             storage.StateStorer
+	chaindID          int64
+	beneficiary       common.Address // the beneficiary we expect in cheques sent to us
+	recoverChequeFunc RecoverChequeFunc
 }
 
 type RecoverChequeFunc func(cheque *SignedCheque, chainID int64) (common.Address, error)
@@ -54,30 +51,30 @@ type RecoverChequeFunc func(cheque *SignedCheque, chainID int64) (common.Address
 // NewChequeStore creates new ChequeStore
 func NewChequeStore(
 	store storage.StateStorer,
-	backend transaction.Backend,
 	chainID int64,
 	beneficiary common.Address,
-	simpleSwapBindingFunc SimpleSwapBindingFunc,
 	recoverChequeFunc RecoverChequeFunc) ChequeStore {
 	return &chequeStore{
-		store:                 store,
-		backend:               backend,
-		chaindID:              chainID,
-		simpleSwapBindingFunc: simpleSwapBindingFunc,
-		beneficiary:           beneficiary,
-		recoverChequeFunc:     recoverChequeFunc,
+		store:             store,
+		chaindID:          chainID,
+		beneficiary:       beneficiary,
+		recoverChequeFunc: recoverChequeFunc,
 	}
 }
 
-// lastReceivedChequeKey computes the key where to store the last cheque received from a chequebook.
-func lastReceivedChequeKey(chequebook common.Address) string {
-	return fmt.Sprintf("%s_%x", lastReceivedChequePrefix, chequebook)
+// lastTransferredTrafficChequeKey computes the key where to store the last cheque received from a chainAddress.
+func lastTransferredTrafficChequeKey(chainAddress common.Address) string {
+	return fmt.Sprintf("%s_%x", lastTransferredTrafficChequePrefix, chainAddress)
 }
 
-// LastCheque returns the last cheque we received from a specific chequebook.
-func (s *chequeStore) LastCheque(chequebook common.Address) (*SignedCheque, error) {
+func lastRetrievedTrafficChequeKey(chainAddress common.Address) string {
+	return fmt.Sprintf("%s_%x", lastRetrievedTrafficChequePrefix, chainAddress)
+}
+
+// LastCheque returns the last cheque we received from a specific chainAddress.
+func (s *chequeStore) LastCheque(chainAddress common.Address) (*SignedCheque, error) {
 	var cheque *SignedCheque
-	err := s.store.Get(lastReceivedChequeKey(chequebook), &cheque)
+	err := s.store.Get(lastTransferredTrafficChequeKey(chainAddress), &cheque)
 	if err != nil {
 		if err != storage.ErrNotFound {
 			return nil, err
@@ -96,14 +93,14 @@ func (s *chequeStore) ReceiveCheque(ctx context.Context, cheque *SignedCheque) (
 	}
 
 	// don't allow concurrent processing of cheques
-	// this would be sufficient on a per chequebook basis
+	// this would be sufficient on a per chainAddress basis
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	// load the lastCumulativePayout for the cheques chequebook
+	// load the lastCumulativePayout for the cheques chainAddress
 	var lastCumulativePayout *big.Int
 	var lastReceivedCheque *SignedCheque
-	err := s.store.Get(lastReceivedChequeKey(cheque.Beneficiary), &lastReceivedCheque)
+	err := s.store.Get(lastRetrievedTrafficChequeKey(cheque.Beneficiary), &lastReceivedCheque)
 	if err != nil {
 		if err != storage.ErrNotFound {
 			return nil, err
@@ -123,51 +120,8 @@ func (s *chequeStore) ReceiveCheque(ctx context.Context, cheque *SignedCheque) (
 
 	// blockchain calls below
 
-	binding, err := s.simpleSwapBindingFunc(cheque.Beneficiary, s.backend)
-	if err != nil {
-		return nil, err
-	}
-
-	// this does not change for the same chequebook
-	expectedIssuer, err := binding.Issuer(&bind.CallOpts{
-		Context: ctx,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// verify the cheque signature
-	issuer, err := s.recoverChequeFunc(cheque, s.chaindID)
-	if err != nil {
-		return nil, err
-	}
-
-	if issuer != expectedIssuer {
-		return nil, ErrChequeInvalid
-	}
-
-	// basic liquidity check
-	// could be omitted as it is not particularly useful
-	balance, err := binding.Balance(&bind.CallOpts{
-		Context: ctx,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	alreadyPaidOut, err := binding.PaidOut(&bind.CallOpts{
-		Context: ctx,
-	}, s.beneficiary)
-	if err != nil {
-		return nil, err
-	}
-
-	if balance.Cmp(big.NewInt(0).Sub(cheque.CumulativePayout, alreadyPaidOut)) < 0 {
-		return nil, ErrBouncingCheque
-	}
-
 	// store the accepted cheque
-	err = s.store.Put(lastReceivedChequeKey(cheque.Beneficiary), cheque)
+	err = s.store.Put(lastRetrievedTrafficChequeKey(cheque.Beneficiary), cheque)
 	if err != nil {
 		return nil, err
 	}
@@ -194,8 +148,8 @@ func RecoverCheque(cheque *SignedCheque, chaindID int64) (common.Address, error)
 	return issuer, nil
 }
 
-// keyChequebook computes the chequebook a store entry is for.
-func keyChequebook(key []byte, prefix string) (chequebook common.Address, err error) {
+// keyChequebook computes the chainAddress a store entry is for.
+func keyChainAddress(key []byte, prefix string) (chainAddress common.Address, err error) {
 	k := string(key)
 
 	split := strings.SplitAfter(k, prefix)
@@ -205,11 +159,11 @@ func keyChequebook(key []byte, prefix string) (chequebook common.Address, err er
 	return common.HexToAddress(split[1]), nil
 }
 
-// LastCheques returns the last received cheques from every known chequebook.
+// LastCheques returns the last received cheques from every known chainAddress.
 func (s *chequeStore) LastCheques() (map[common.Address]*SignedCheque, error) {
 	result := make(map[common.Address]*SignedCheque)
-	err := s.store.Iterate(lastReceivedChequePrefix, func(key, val []byte) (stop bool, err error) {
-		addr, err := keyChequebook(key, lastReceivedChequePrefix+"_")
+	err := s.store.Iterate(lastTransferredTrafficChequePrefix, func(key, val []byte) (stop bool, err error) {
+		addr, err := keyChainAddress(key, lastTransferredTrafficChequePrefix+"_")
 		if err != nil {
 			return false, fmt.Errorf("parse address from key: %s: %w", string(key), err)
 		}
