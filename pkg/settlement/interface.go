a257b5d474794de1e72b1ffe0178fc26fcd70d3c
lewis <Lewis@aufs.io>
2021-11-26 18:08:47 +0800

    feat(traffic): remove redundant traffic interfaces.

--- pkg/settlement/interface.go
+++ pkg/settlement/interface.go
@@ -17,24 +17,18 @@ type Interface interface {
 	// Pay initiates a payment to the given peer
 	// It should return without error it is likely that the payment worked
 	Pay(ctx context.Context, peer boson.Address, traffic, paymentThreshold *big.Int) error
+
 	TransferTraffic(peer boson.Address) (traffic *big.Int, err error)
+
 	RetrieveTraffic(peer boson.Address) (traffic *big.Int, err error)
+
 	PutRetrieveTraffic(peer boson.Address, traffic *big.Int) error
+
 	PutTransferTraffic(peer boson.Address, traffic *big.Int) error
-	// TotalSent returns the total amount sent to a peer
-	TotalSent(peer boson.Address) (totalSent *big.Int, err error)
-	// TotalReceived returns the total amount received from a peer
-	TotalReceived(peer boson.Address) (totalReceived *big.Int, err error)
-	// SettlementsSent returns sent settlements for each individual known peer
-	SettlementsSent() (map[string]*big.Int, error)
-	// SettlementsReceived returns received settlements for each individual known peer
-	SettlementsReceived() (map[string]*big.Int, error)
-	// Balance Get chain balance
-	Balance(ctx context.Context) (*big.Int, error)
+
 	// AvailableBalance Get actual available balance
 	AvailableBalance(ctx context.Context) (*big.Int, error)
 
-	UpdatePeerBalance(peer boson.Address) error
 	// SetNotifyPaymentFunc sets the NotifyPaymentFunc to notify
 	SetNotifyPaymentFunc(notifyPaymentFunc NotifyPaymentFunc)
 }
