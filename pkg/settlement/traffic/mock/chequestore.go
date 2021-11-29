3fbd12fb30d2dadfcce6cd074dcb55e1b39b9f5a
vicentaufs <vicent@aufs.io>
2021-11-29 13:11:06 +0800

    Feat: add chequestore Mock

---
+++ pkg/settlement/traffic/mock/chequestore.go
@@ -0,0 +1,141 @@
+package mock
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/gauss-project/aurorafs/pkg/settlement/traffic/cheque"
+	"math/big"
+)
+
+// Service is the mock chequeStore service.
+type ChequeStoreMock struct {
+	// ReceiveCheque verifies and stores a cheque. It returns the totam amount earned.
+	receiveCheque func(ctx context.Context, cheque *cheque.SignedCheque) (*big.Int, error)
+
+	putSendCheque func(ctx context.Context, cheque *cheque.Cheque, chainAddress common.Address) error
+	// LastReceivedCheque returns the last cheque we received from a specific chainAddress.
+	lastReceivedCheque func(chainAddress common.Address) (*cheque.SignedCheque, error)
+	// LastReceivedCheques returns the last received cheques from every known chainAddress.
+	lastReceivedCheques func() (map[common.Address]*cheque.SignedCheque, error)
+
+	lastSendCheque func(chainAddress common.Address) (*cheque.Cheque, error)
+
+	lastSendCheques func() (map[common.Address]*cheque.Cheque, error)
+
+	putRetrieveTraffic func(chainAddress common.Address, traffic *big.Int) error
+
+	putTransferTraffic func(chainAddress common.Address, traffic *big.Int) error
+
+	putReceivedCheques func(chainAddress common.Address, cheque cheque.SignedCheque) error
+}
+
+func WithRetrieveChequeFunc(f func(ctx context.Context, cheque *cheque.SignedCheque) (*big.Int, error)) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.receiveCheque = f
+	})
+}
+
+func WithPutSendCheque(f func(ctx context.Context, cheque *cheque.Cheque, chainAddress common.Address) error) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.putSendCheque = f
+	})
+}
+
+func WithLastReceivedCheque(f func(chainAddress common.Address) (*cheque.SignedCheque, error)) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.lastReceivedCheque = f
+	})
+}
+
+func WithLastReceivedCheques(f func() (map[common.Address]*cheque.SignedCheque, error)) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.lastReceivedCheques = f
+	})
+}
+
+func WithLastSendCheque(f func(chainAddress common.Address) (*cheque.Cheque, error)) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.lastSendCheque = f
+	})
+}
+
+func WithLastSendCheques(f func() (map[common.Address]*cheque.Cheque, error)) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.lastSendCheques = f
+	})
+}
+
+func WithPutRetrieveTraffic(f func(chainAddress common.Address, traffic *big.Int) error) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.putRetrieveTraffic = f
+	})
+}
+
+func WithPutTransferTraffic(f func(chainAddress common.Address, traffic *big.Int) error) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.putTransferTraffic = f
+	})
+}
+
+func WithPutReceivedCheques(f func(chainAddress common.Address, cheque cheque.SignedCheque) error) Option {
+	return optionFunc(func(s *ChequeStoreMock) {
+		s.putReceivedCheques = f
+	})
+}
+
+// NewChequeStore creates the mock chequeStore implementation
+func NewChequeStore(opts ...Option) cheque.ChequeStore {
+	mock := new(ChequeStoreMock)
+	for _, o := range opts {
+		o.apply(mock)
+	}
+	return mock
+}
+
+// ReceiveCheque verifies and stores a cheque. It returns the totam amount earned.
+func (s *ChequeStoreMock) ReceiveCheque(ctx context.Context, cheque *cheque.SignedCheque) (*big.Int, error) {
+	return s.receiveCheque(ctx, cheque)
+}
+
+func (s *ChequeStoreMock) PutSendCheque(ctx context.Context, cheque *cheque.Cheque, chainAddress common.Address) error {
+	return s.putSendCheque(ctx, cheque, chainAddress)
+}
+
+// LastReceivedCheque returns the last cheque we received from a specific chainAddress.
+func (s *ChequeStoreMock) LastReceivedCheque(chainAddress common.Address) (*cheque.SignedCheque, error) {
+	return s.lastReceivedCheque(chainAddress)
+}
+
+// LastReceivedCheques returns the last received cheques from every known chainAddress.
+func (s *ChequeStoreMock) LastReceivedCheques() (map[common.Address]*cheque.SignedCheque, error) {
+	return s.lastReceivedCheques()
+}
+
+func (s *ChequeStoreMock) LastSendCheque(chainAddress common.Address) (*cheque.Cheque, error) {
+	return s.lastSendCheque(chainAddress)
+}
+
+func (s *ChequeStoreMock) LastSendCheques() (map[common.Address]*cheque.Cheque, error) {
+	return s.lastSendCheques()
+}
+
+func (s *ChequeStoreMock) PutRetrieveTraffic(chainAddress common.Address, traffic *big.Int) error {
+	return s.putRetrieveTraffic(chainAddress, traffic)
+}
+
+func (s *ChequeStoreMock) PutTransferTraffic(chainAddress common.Address, traffic *big.Int) error {
+	return s.putTransferTraffic(chainAddress, traffic)
+}
+
+func (s *ChequeStoreMock) PutReceivedCheques(chainAddress common.Address, cheque cheque.SignedCheque) error {
+	return s.putReceivedCheques(chainAddress, cheque)
+}
+
+// Option is the option passed to the mock ChequeStore service
+type Option interface {
+	apply(*ChequeStoreMock)
+}
+
+type optionFunc func(*ChequeStoreMock)
+
+func (f optionFunc) apply(r *ChequeStoreMock) { f(r) }
--- pkg/settlement/traffic/mock/traffic.go
+++ pkg/settlement/traffic/mock/traffic.go
@@ -7,63 +7,41 @@ import (
 	"github.com/gauss-project/aurorafs/pkg/settlement/traffic/cheque"
 )
 
-type trafficAddressBookMock struct {
+type TrafficAddressBookMock struct {
 	beneficiary     func(peer boson.Address) (beneficiary common.Address, known bool, err error)
 	beneficiaryPeer func(beneficiary common.Address) (peer boson.Address, known bool, err error)
 	putBeneficiary  func(peer boson.Address, beneficiary common.Address) error
 	initAddressBook func() error
 }
 
-func (m *trafficAddressBookMock) Beneficiary(peer boson.Address) (beneficiary common.Address, known bool, err error) {
+func (m *TrafficAddressBookMock) Beneficiary(peer boson.Address) (beneficiary common.Address, known bool, err error) {
 	return m.beneficiary(peer)
 }
 
-func (m *trafficAddressBookMock) BeneficiaryPeer(beneficiary common.Address) (peer boson.Address, known bool, err error) {
+func (m *TrafficAddressBookMock) BeneficiaryPeer(beneficiary common.Address) (peer boson.Address, known bool, err error) {
 	return m.beneficiaryPeer(beneficiary)
 }
 
-func (m *trafficAddressBookMock) PutBeneficiary(peer boson.Address, beneficiary common.Address) error {
+func (m *TrafficAddressBookMock) PutBeneficiary(peer boson.Address, beneficiary common.Address) error {
 	return m.putBeneficiary(peer, beneficiary)
 }
 
-func (m *trafficAddressBookMock) InitAddressBook() error {
+func (m *TrafficAddressBookMock) InitAddressBook() error {
 	return nil
 }
 
-type cashOutMock struct {
+type CashOutMock struct {
 	cashCheque func(ctx context.Context, chequebook common.Address, recipient common.Address) (common.Hash, error)
 }
 
-func (m *cashOutMock) CashCheque(ctx context.Context, chequebook common.Address, recipient common.Address) (common.Hash, error) {
+func (m *CashOutMock) CashCheque(ctx context.Context, chequebook common.Address, recipient common.Address) (common.Hash, error) {
 	return m.cashCheque(ctx, chequebook, recipient)
 }
 
-type chequeSignerMock struct {
+type ChequeSignerMock struct {
 	sign func(cheque *cheque.Cheque) ([]byte, error)
 }
 
-func (m *chequeSignerMock) Sign(cheque *cheque.Cheque) ([]byte, error) {
+func (m *ChequeSignerMock) Sign(cheque *cheque.Cheque) ([]byte, error) {
 	return m.sign(cheque)
 }
-
-//
-//type ChequeStore interface {
-//	// ReceiveCheque verifies and stores a cheque. It returns the totam amount earned.
-//	ReceiveCheque(ctx context.Context, cheque *SignedCheque) (*big.Int, error)
-//	// LastCheque returns the last cheque we received from a specific chequebook.
-//	LastCheque(chequebook common.Address) (*SignedCheque, error)
-//	// LastCheques returns the last received cheques from every known chequebook.
-//	LastCheques() (map[common.Address]*SignedCheque, error)
-//}
-
-//func (s *Service) ReceiveCheque(ctx context.Context, cheque *chequebook.SignedCheque) (*big.Int, error) {
-//	return s.receiveCheque(ctx, cheque)
-//}
-//
-//func (s *Service) LastCheque(chequebook common.Address) (*chequebook.SignedCheque, error) {
-//	return s.lastCheque(chequebook)
-//}
-//
-//func (s *Service) LastCheques() (map[common.Address]*chequebook.SignedCheque, error) {
-//	return s.lastCheques()
-//}
