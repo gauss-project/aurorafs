226bae890c0a4ba0ff4921e9bca80b2a53ce8cc1
vicentaufs <vicent@aufs.io>
2021-12-03 18:17:13 +0800

    fix: pay optimization

--- pkg/settlement/pseudosettle/pseudosettle.go
+++ pkg/settlement/pseudosettle/pseudosettle.go
@@ -133,9 +133,9 @@ func (s *Service) handler(ctx context.Context, p p2p.Peer, stream p2p.Stream) (e
 }
 
 // Pay initiates a payment to the given peer
-func (s *Service) Pay(ctx context.Context, peer boson.Address, traffic, paymentThreshold *big.Int) error {
+func (s *Service) Pay(ctx context.Context, peer boson.Address, paymentThreshold *big.Int) error {
 
-	return s.notifyPaymentFunc(peer, traffic)
+	return nil
 
 }
 
