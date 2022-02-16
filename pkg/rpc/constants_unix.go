6fac30eb5bd5bde5deed2a957bc62e28580fb144
IrvinAufs <88412010+IrvinAufs@users.noreply.github.com>
2022-02-16 17:34:32 +0800

    fix: duplicate symbol: max_socket_path_size (#153)
    rename function in rpc pkg

--- pkg/rpc/constants_unix.go
+++ pkg/rpc/constants_unix.go
@@ -22,13 +22,13 @@ package rpc
 /*
 #include <sys/un.h>
 
-int max_socket_path_size() {
-struct sockaddr_un s;
-return sizeof(s.sun_path);
+int rpc_max_socket_path_size() {
+	struct sockaddr_un s;
+	return sizeof(s.sun_path);
 }
 */
 import "C"
 
 var (
-	max_path_size = C.max_socket_path_size()
+	max_path_size = C.rpc_max_socket_path_size()
 )
