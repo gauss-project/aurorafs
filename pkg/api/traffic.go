5772d194c0cb84b32b7664fc3e4f42eb7c4bf3ea
LewisAufs <Lewis@aufs.io>
2022-04-20 15:58:27 +0800

    perf: traffic sort.

--- pkg/api/traffic.go
+++ pkg/api/traffic.go
@@ -76,7 +76,7 @@ func (s *server) trafficCheques(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sort.Slice(chequeList, func(i, j int) bool {
-		return chequeList[i].Total.Cmp(chequeList[j].Total) > 0
+		return chequeList[i].UnCashed.Cmp(chequeList[j].UnCashed) > 0
 	})
 	jsonhttp.OK(w, chequeList)
 }
